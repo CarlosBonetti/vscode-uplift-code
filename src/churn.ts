bff9f4e69af5d73a8ad8b27f38f3ec3c531076c6
Carlos Bonetti <carlosbonetti.mail@gmail.com>
2021-11-03 20:27:29 -0300

    Add exclude patterns filter to project churn calculation

--- src/churn.ts
+++ src/churn.ts
@@ -1,52 +1,69 @@
 import { SimpleGit } from "simple-git";
+import minimatch from "minimatch";
 
 const since = "18.month";
 
-export function calculateProjectChurn(
-  git: SimpleGit
-): Promise<Map<string, number>> {
-  return new Promise((resolve, reject) => {
-    git
-      .raw(
-        "log",
-        "--all",
-        "-M",
-        "-C",
-        "--format=format:",
-        "--name-only",
-        `--since=${since}`
-      )
-      .then((result) => {
-        const map = extractResultLines(result).reduce((map, fileName) => {
-          map.set(fileName, (map.get(fileName) ?? 0) + 1);
-          return map;
-        }, new Map<string, number>());
+// TODO: create an extension config
+const excludePatterns = [
+  "**/package.json",
+  "**/package-lock.json",
+  "**/poetry.lock",
+  "frontend/projects/main/src/locale/*",
+  "docker/frontend/eslint/eslint_count_errors.sh",
+  "docker/frontend/eslint/base_lint_report.txt",
+];
 
-        resolve(map);
-      });
-  });
+function createExcludePatternsFilter(patterns: string[]) {
+  const filters = patterns.map((pattern) => minimatch.filter(pattern));
+  return (target: string, index: number, array: string[]) =>
+    filters.every((filter) => !filter(target, index, array));
 }
 
-export function calculateFileChurn(
+export async function calculateProjectChurn(
+  git: SimpleGit
+): Promise<Map<string, number>> {
+  const result = await execRawGitCommand(git, [
+    "log",
+    "--all",
+    "--find-renames",
+    "--find-copies",
+    "--name-only",
+    "--format=format:",
+    `--since=${since}`,
+  ]);
+
+  const filterExcludePatterns = createExcludePatternsFilter(excludePatterns);
+
+  const map = result.filter(filterExcludePatterns).reduce((map, fileName) => {
+    map.set(fileName, (map.get(fileName) ?? 0) + 1);
+    return map;
+  }, new Map<string, number>());
+
+  return map;
+}
+
+export async function calculateFileChurn(
   git: SimpleGit,
   fileName: string
 ): Promise<number> {
-  return new Promise((resolve, reject) => {
-    git
-      .raw(
-        "log",
-        "--format=format:",
-        "--name-only",
-        "--follow",
-        `--since=${since}`,
-        fileName
-      )
-      .then((result) => {
-        const churn = extractResultLines(result).length;
+  const result = await execRawGitCommand(git, [
+    "log",
+    "--format=format:",
+    "--name-only",
+    "--follow",
+    `--since=${since}`,
+    fileName,
+  ]);
 
-        resolve(churn);
-      });
-  });
+  return result.length;
+}
+
+async function execRawGitCommand(
+  git: SimpleGit,
+  commands: string[]
+): Promise<string[]> {
+  const result = await git.raw(commands);
+  return extractResultLines(result);
 }
 
 function extractResultLines(result: string): string[] {
